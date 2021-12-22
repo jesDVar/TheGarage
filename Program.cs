71afe8bf566192ec03c449fe496ec3e2f27b9f6c
Jesse Varis <jessevaris@hotmail.com>
2021-12-22 16:44:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
