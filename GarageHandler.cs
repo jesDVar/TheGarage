fa6b4911c2765bf88ef2d176020b553691b8ef01
Jesse Varis <jessevaris@hotmail.com>
2021-12-28 16:13:02 +0100

    Able to park a dummy vehicle

--- GarageHandler.cs
+++ GarageHandler.cs
@@ -53,8 +53,7 @@ namespace TheGarage
             // want to park(Car, Bus Airplane...)?
 
             //Console.WriteLine("Are You TRying To PARK?");
-            Vehicle vehicle = new Vehicle();
-
+            garage.Park(new MotorCycle("MOT111", "Red", 2, "Light", 1));
 
         }
 
